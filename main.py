2ff4fe1e3dca7ce44f1a3d772af85831eaee9e08
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-21 18:34:19 +0530

    :sparkles: Implemented Twitter Automation

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-from lib.twitter_poster import post_twitter
-from lib.youtube import get_latest_video
-import json
-import datetime
+from server import keep_alive
+from automation import automate
 
-# video_title='Top 10 Wholesome anime for Christmas'
-# video_link = 'https://youtu.be/hpqmFF1P-NA'
 
-# post_twitter(video_title, video_link)
 
-def automate_twitter():
-    with open('data.json','r') as f:
-        data=json.load(f)
-        past_title = data["title"]
-
-    new_title = get_latest_video()[0]
-    url = get_latest_video()[1]
-    if past_title != new_title:
-        data["title"]=new_title
-        data["url"] = url
-        data["last_updated"]=str(datetime.date.today())
-        with open("data.json", "w") as f:
-            json.dump(data,f)
-        print(new_title)
-
-automate_twitter()
-print("Tweet Send")
+keep_alive()
+automate.run_automation()
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def main():
     return "Your bot is alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
 
 def keep_alive():
     server = Thread(target=run)
