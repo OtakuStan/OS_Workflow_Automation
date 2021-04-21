1bb82cc4d42fe19681140f518dbdd508b445ec46
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-21 16:58:53 +0530

    :zap: getting Youtube video details

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from lib.twitter_poster import post_twitter
+from lib.youtube import get_latest_video
 
-video_title='Top 10 Wholesome anime for Christmas'
-video_link = 'https://youtu.be/hpqmFF1P-NA'
+# video_title='Top 10 Wholesome anime for Christmas'
+# video_link = 'https://youtu.be/hpqmFF1P-NA'
 
-post_twitter(video_title, video_link)
+# post_twitter(video_title, video_link)
+
+print(get_latest_video())
 
 print("Tweet Send")
