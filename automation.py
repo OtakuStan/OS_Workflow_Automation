e006ff04aad6ce5a057dae6d47131e3f5bd6cc3a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-23 14:09:21 +0530

    :ambulance: Youtube: Introduced error handeling in api requests

--- automation.py
+++ automation.py
@@ -10,30 +10,29 @@ class Automate_Twitter():
         self.version = version
 
     def automating_twitter(self):
-        print("Getting Tweet Data")
         with open('data.json','r') as f:
             data=json.load(f)
             past_title = data["title"]
 
         new_title = get_latest_video()[0]
         url = get_latest_video()[1]
-        if past_title != new_title:
-            data["title"]=new_title
-            data["url"] = url
-            data["last_updated"]=str(datetime.date.today())
-            with open("data.json", "w") as f:
-                json.dump(data,f)
-            post_twitter(new_title,url)
-            print(new_title)
-        else:
-            print("No New Video")
+        if new_title is not None and url is not None:
+          if past_title != new_title:
+              data["title"]=new_title
+              data["url"] = url
+              data["last_updated"]=str(datetime.date.today())
+              with open("data.json", "w") as f:
+                  json.dump(data,f)
+              post_twitter(new_title,url)
+          else:
+              print("No New Video")
 
     def run_automation(self):
         starttime = time.time()
         while True:
             print ("tick")
             self.automating_twitter()
-            time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+            time.sleep(900.0 - ((time.time() - starttime) % 900.0))
 
 
 automate = Automate_Twitter(version=VERSION)
--- lib/youtube.py
+++ lib/youtube.py
@@ -7,10 +7,18 @@ yt_channel_id = "UCcvnNBCZplzRkGCFyzNcwzg"
 yt_base_url = "https://www.youtube.com/watch?v="
 
 def get_latest_video() -> tuple:
-  latest_video = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={yt_channel_id}&part=snippet,id&order=date&maxResults=1"
-  recv=requests.get(latest_video)
-  js = recv.json()["items"][0]
+  title = None
+  url = None
+  try:
+    latest_video = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={yt_channel_id}&part=snippet,id&order=date&maxResults=1"
+    recv=requests.get(latest_video)
+    if "items" in recv.json():
+      js = recv.json()["items"][0]
 
-  title = js["snippet"]["title"]
-  url = yt_base_url + js["id"]["videoId"]
-  return (title,url)
+      title = js["snippet"]["title"]
+      url = yt_base_url + js["id"]["videoId"]
+      return (title,url)
+    else:
+      return (title,url)
+  except Exception as e:
+    raise e
